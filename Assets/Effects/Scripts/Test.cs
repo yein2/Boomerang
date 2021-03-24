161566c0c9a90015a44b9e1760c656d7d544ca63
cyi <chldlsa123@gmail.com>
2021-03-24 18:04:52 +0900

    Effect_1

--- Assets/Effects/Scripts/Test.cs
+++ Assets/Effects/Scripts/Test.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    [SerializeField]
+    MeshRenderer meshTest;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-     
+      if(Input.GetKeyDown(KeyCode.A))
+      {
+          StartCoroutine(DissolveShield());
+      }
+    }
+
+    IEnumerator DissolveShield()    // 쉴드가 점점 사라지는 효과
+    {
+        float amount = -1f;
+        while(amount <= 1)
+        {
+            amount += 0.1f;
+            meshTest.material.SetFloat("Vector1_40E1A2BF", amount);
+            //Debug.Log(meshTest.material.GetFloat("Vector1_40E1A2BF"));
+            yield return new WaitForSeconds(0.05f); 
+        }
+        // 후처리
+        yield break;
     }
 }
